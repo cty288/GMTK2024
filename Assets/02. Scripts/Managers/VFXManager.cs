64d97b70080611c98ced55d89b0fd5eb36cbe94d
Ryan Zhang <zhangryan09@gmail.com>
2024-08-18 15:17:57 -0400

    dust vfx and fixed rings being marked as volva

---
+++ Assets/02. Scripts/Managers/VFXManager.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VFXManager : MonoBehaviour
+{
+    private static VFXManager _instance;
+
+    public static VFXManager Instance
+    {
+        get
+        {
+            return _instance;
+        }
+    }
+
+    [SerializeField] private ParticleSystem dustPlace;
+    [SerializeField] private ParticleSystem dustPickUp;
+
+    private void Awake()
+    {
+        _instance = this;
+    }
+}
