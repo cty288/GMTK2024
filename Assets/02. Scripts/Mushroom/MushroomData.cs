84c7248ddbe0ab1f3c42854cd1a6c7ffb17a7ace
Allyson <abc9295@nyu.edu>
2024-08-18 01:19:15 -0700

    mushroom stages WIP

--- Assets/02. Scripts/Mushroom/MushroomData.cs
+++ Assets/02. Scripts/Mushroom/MushroomData.cs
@@ -1,12 +1,9 @@
+using MikroFramework.BindableProperty;
+using NHibernate.Util;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using MikroFramework.Architecture;
-using MikroFramework.BindableProperty;
-using MikroFramework.Event;
-using NHibernate.Util;
 using UnityEngine;
-using Action = Antlr.Runtime.Misc.Action;
 using Random = UnityEngine.Random;
 
 public enum MushroomPropertyTag {
@@ -25,11 +22,11 @@ public enum MushroomPropertyTag {
 public interface IMushroomProperty {
     HashSet<MushroomPropertyTag> Tags { get; }
 }
-public class MushroomProperty<T> : IMushroomProperty{
+public class MushroomProperty<T> : IMushroomProperty {
     public T BaseValue { get; set; }
-    
+
     public BindableProperty<T> RealValue { get; }
-    
+
     public HashSet<MushroomPropertyTag> Tags { get; }
 
     public MushroomProperty(T baseValue, params MushroomPropertyTag[] tags) {
@@ -68,48 +65,48 @@ public class MushroomData {
     public MushroomProperty<Color> stemColor1;
     public MushroomProperty<bool> isPoisonous;
     public MushroomProperty<float> sporeRange;
-    
-    
+
+
     public BindableProperty<int> GrowthDay { get; } = new BindableProperty<int>(1);
 
     public int GetStage() {
         return GetStage(GrowthDay.Value);
     }
-    
+
     public int GetStage(int day) {
         if (day <= 2) {
             return 1;
-        }else if (day <= 4) {
+        } else if (day <= 4) {
             return 2;
-        }else if (day <= 5) {
+        } else if (day <= 5) {
             return 3;
         }
 
         return 4;
     }
-    
-    public MushroomData() :  this(1, 1, 1, 1, 1, new Vector2(1, 1), 1, Color.white, Color.white, Color.white, Color.white, Color.white, Color.white, false, 1) {
-        
+
+    public MushroomData() : this(1, 1, 1, 1, 1, new Vector2(1, 1), 1, Color.white, Color.white, Color.white, Color.white, Color.white, Color.white, false, 1) {
+
         AddBasicProperties();
     }
-    
+
     public MushroomData(float capHeight, float capWidth, float stemHeight, float stemWidth, float growthSpeed, Vector2 oscillation, float oscillationSpeed, Color capColor, Color stemColor, Color capColor0, Color stemColor0, Color capColor1, Color stemColor1, bool isPoisonous, float sporeRange) {
-        this.capHeight = new MushroomProperty<float>(capHeight,  MushroomPropertyTag.Cap, MushroomPropertyTag.Height, MushroomPropertyTag.Size);
+        this.capHeight = new MushroomProperty<float>(capHeight, MushroomPropertyTag.Cap, MushroomPropertyTag.Height, MushroomPropertyTag.Size);
         this.capWidth = new MushroomProperty<float>(capWidth, MushroomPropertyTag.Cap, MushroomPropertyTag.Width, MushroomPropertyTag.Size);
-        this.stemHeight = new MushroomProperty<float>(stemHeight,  MushroomPropertyTag.Stem, MushroomPropertyTag.Height, MushroomPropertyTag.Size);
+        this.stemHeight = new MushroomProperty<float>(stemHeight, MushroomPropertyTag.Stem, MushroomPropertyTag.Height, MushroomPropertyTag.Size);
         this.stemWidth = new MushroomProperty<float>(stemWidth, MushroomPropertyTag.Stem, MushroomPropertyTag.Width, MushroomPropertyTag.Size);
         this.oscillation = new MushroomProperty<Vector2>(oscillation, MushroomPropertyTag.Oscillation);
         this.oscillationSpeed = new MushroomProperty<float>(oscillationSpeed, MushroomPropertyTag.OscillationSpeed);
-        this.capColor = new MushroomProperty<Color>(capColor, MushroomPropertyTag.Cap,MushroomPropertyTag.Color);
+        this.capColor = new MushroomProperty<Color>(capColor, MushroomPropertyTag.Cap, MushroomPropertyTag.Color);
         this.stemColor = new MushroomProperty<Color>(stemColor, MushroomPropertyTag.Stem, MushroomPropertyTag.Color);
-        this.capColor0 = new MushroomProperty<Color>(capColor0, MushroomPropertyTag.Cap,MushroomPropertyTag.Color);
+        this.capColor0 = new MushroomProperty<Color>(capColor0, MushroomPropertyTag.Cap, MushroomPropertyTag.Color);
         this.stemColor0 = new MushroomProperty<Color>(stemColor0, MushroomPropertyTag.Stem, MushroomPropertyTag.Color);
         this.capColor1 = new MushroomProperty<Color>(capColor1, MushroomPropertyTag.Cap, MushroomPropertyTag.Color);
         this.stemColor1 = new MushroomProperty<Color>(stemColor1, MushroomPropertyTag.Stem, MushroomPropertyTag.Color);
         this.isPoisonous = new MushroomProperty<bool>(isPoisonous, MushroomPropertyTag.Poisonous);
         this.sporeRange = new MushroomProperty<float>(sporeRange, MushroomPropertyTag.SporeRange);
         this.growthSpeed = new MushroomProperty<float>(growthSpeed, MushroomPropertyTag.Growth);
-        
+
         AddBasicProperties();
     }
 
@@ -133,29 +130,29 @@ public class MushroomData {
     public int GetSellPrice() { //TODO: later
         return 2;
     }
-    
+
     public int GetBuyPrice() { //TODO: later
         return 1;
     }
-    
+
     public void AddProperty(IMushroomProperty property) {
 
         property.Tags.ForEach(tag => {
             if (!properties.ContainsKey(tag)) {
                 properties.Add(tag, new List<IMushroomProperty>());
             }
-            
+
             /*if(properties[tag].Count > 0 && properties[tag][0].GetType() != property.GetType()) {
                 throw new System.Exception("Property type mismatch for tag " + tag + " in part " + part + " : " + property.GetType() + " != " + properties[part][tag][0].GetType() + "");
             }*/
-            
+
             properties[tag].Add(property);
         });
-        
-  
+
+
         flattenedProperties.Add(property);
     }
-    
+
     /*public List<MushroomProperty<T>> GetProperties<T>(ShroomPart part, MushroomPropertyTag tag) {
         if (!properties.ContainsKey(part) || !properties[part].ContainsKey(tag)) {
             return new List<MushroomProperty<T>>();
@@ -175,43 +172,41 @@ public class MushroomData {
 
         return result;
     }*/
-    
+
     public void AddTrait<T>(MushroomTrait trait) {
-        AddTrait((IMushroomTrait) trait);
+        AddTrait((IMushroomTrait)trait);
     }
-    
+
     public void AddTrait(IMushroomTrait trait) {
         trait.OnStartApply(this);
         traits.Add(trait.GetType(), trait);
         if (traitAddCallbacks.ContainsKey(trait.GetType())) {
             traitAddCallbacks[trait.GetType()](trait);
         }
-        
+
     }
-    
+
     private Dictionary<object, Action<IMushroomTrait>> traitRemoveCallbacks = new Dictionary<object, Action<IMushroomTrait>>();
     public void RegisterOnTraitAdd<T>(Action<T> callback, bool callIfExist = true) where T : IMushroomTrait {
-        Action<IMushroomTrait> action = trait => callback((T) trait);
+        Action<IMushroomTrait> action = trait => callback((T)trait);
         traitRemoveCallbacks.Add(callback, action);
-        
+
         if (!traitAddCallbacks.ContainsKey(typeof(T))) {
             traitAddCallbacks.Add(typeof(T), action);
-        }
-        else {
+        } else {
             traitAddCallbacks[typeof(T)] += action;
         }
         if (callIfExist) {
             if (traits.ContainsKey(typeof(T))) {
-                callback((T) traits[typeof(T)]);
+                callback((T)traits[typeof(T)]);
             }
         }
     }
-    
+
     public void RegisterOnTraitAdd(Action<IMushroomTrait> callback, bool callIfExist = true) {
         if (onTraitAdd == null) {
             onTraitAdd = callback;
-        }
-        else {
+        } else {
             onTraitAdd += callback;
         }
         if (callIfExist) {
@@ -220,28 +215,28 @@ public class MushroomData {
             }
         }
     }
-    
+
     public void UnregisterOnTraitAdd(Action<IMushroomTrait> callback) {
         onTraitAdd -= callback;
     }
-    
+
     public void UnregisterOnTraitAdd<T>(Action<T> callback) where T : IMushroomTrait {
         Action<IMushroomTrait> action = traitRemoveCallbacks[callback];
         traitAddCallbacks[typeof(T)] -= action;
         traitRemoveCallbacks.Remove(callback);
     }
-    
+
     public List<IMushroomTrait> GetTraits() {
         return new List<IMushroomTrait>(traits.Values);
     }
-    
+
     public bool HasTrait<T>() where T : IMushroomTrait {
         return traits.ContainsKey(typeof(T));
     }
-    
+
     public bool HasTrait<T>(out T trait) where T : IMushroomTrait {
         if (traits.ContainsKey(typeof(T))) {
-            trait = (T) traits[typeof(T)];
+            trait = (T)traits[typeof(T)];
             return true;
         }
 
@@ -262,10 +257,10 @@ public class MushroomData {
             if (!properties.ContainsKey(tag)) {
                 return null;
             }
-            
+
             result.UnionWith(properties[tag]);
         }
-        
+
         //filter out those of different types
         result.RemoveWhere(property => property.GetType() != typeof(MushroomProperty<T>));
         return new HashSet<MushroomProperty<T>>(result.Cast<MushroomProperty<T>>());
@@ -277,8 +272,8 @@ public static class MushroomDataHelper {
     public static MushroomData GetControlMushroomData() {
         return new MushroomData();
     }
-    
-    
+
+
     public static MushroomData GetInitialMushroomData() {
         MushroomData data = new MushroomData(
             Random.Range(0.5f, 1),
@@ -299,7 +294,7 @@ public static class MushroomDataHelper {
 
         return data;
     }
-    
+
     public static MushroomData GetRandomMushroomData(int initialGrowthDay, int minTraitCount, int maxTraitCount) {
         MushroomData data = new MushroomData(
             Random.Range(0.3f, 1.8f),
@@ -318,12 +313,12 @@ public static class MushroomDataHelper {
             Random.value > 0.5f,
             Random.Range(0.8f, 1.6f));
         data.GrowthDay.Value = initialGrowthDay;
-        
+
         int traitCount = Random.Range(minTraitCount, maxTraitCount + 1);
         var traits = TraitPool.GetRandomTraits(traitCount);
 
-        
-        
+
+
         foreach (var trait in traits) {
             data.AddTrait(trait);
         }
@@ -344,6 +339,6 @@ public static class MushroomDataHelper {
                 $"Is Poisonous: {data.isPoisonous}\n" +
                 $"Spore Range: {data.sporeRange}";
     }
-    
-    
+
+
 }
